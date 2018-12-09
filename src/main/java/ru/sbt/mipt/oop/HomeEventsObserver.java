1ff0306ee551b03aa800e90da1109d20a13a4bb6
akivo4ka <akivavika@mail.ru>
2018-12-09 16:15:18 +0300

    Move CreateEventProcessorList to MyConfiguration

--- src/main/java/ru/sbt/mipt/oop/HomeEventsObserver.java
+++ src/main/java/ru/sbt/mipt/oop/HomeEventsObserver.java
@@ -1,13 +1,10 @@
 package ru.sbt.mipt.oop;
 
-import ru.sbt.mipt.oop.homeunits.SmartHome;
-import ru.sbt.mipt.oop.processors.*;
-import ru.sbt.mipt.oop.sensoreventprovider.RandomSensorEventProvider;
+import ru.sbt.mipt.oop.processors.EventProcessor;
 import ru.sbt.mipt.oop.sensoreventprovider.SensorEventProvider;
 import ru.sbt.mipt.oop.sensoreventsmanager.SensorEventsManager;
 import ru.sbt.mipt.oop.sensors.SensorEvent;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class HomeEventsObserver implements SensorEventsManager {
@@ -37,16 +34,6 @@ public class HomeEventsObserver implements SensorEventsManager {
         this.sensorEventProvider = sensorEventProvider;
     }
 
-    public HomeEventsObserver(SmartHome smartHome) {
-        this.eventProcessorList = createEventProcessorList(smartHome);
-        this.sensorEventProvider = new RandomSensorEventProvider();
-    }
-
-    HomeEventsObserver(SmartHome smartHome, SensorEventProvider sensorEventProvider) {
-        this.eventProcessorList = createEventProcessorList(smartHome);
-        this.sensorEventProvider = sensorEventProvider;
-    }
-
     public void addEventProcessor(EventProcessor eventProcessor) {
         eventProcessorList.add(eventProcessor);
     }
@@ -56,18 +43,5 @@ public class HomeEventsObserver implements SensorEventsManager {
         for (EventProcessor eventProcessor : eventProcessorList) {
             eventProcessor.process(event);
         }
-    }
-
-    private static List<EventProcessor> createEventProcessorList(SmartHome smartHome) {
-        List<EventProcessor> eventProcessorList = new ArrayList<>();
-        EventProcessor lightEventProcessor = new DecoratorCheckAlarmEventProcessor(new LightEventProcessor(smartHome), smartHome);
-        EventProcessor doorEventProcessor = new DecoratorCheckAlarmEventProcessor(new DoorEventProcessor(smartHome), smartHome);
-        EventProcessor hallDoorEventProcessor = new DecoratorCheckAlarmEventProcessor(new HallDoorEventProcessor(smartHome), smartHome);
-        AlarmEventProcessor alarmEventProcessor = new AlarmEventProcessor(smartHome);
-        eventProcessorList.add(lightEventProcessor);
-        eventProcessorList.add(doorEventProcessor);
-        eventProcessorList.add(hallDoorEventProcessor);
-        eventProcessorList.add(alarmEventProcessor);
-        return eventProcessorList;
     }
 }
--- src/main/java/ru/sbt/mipt/oop/MyConfiguration.java
+++ src/main/java/ru/sbt/mipt/oop/MyConfiguration.java
@@ -3,11 +3,14 @@ package ru.sbt.mipt.oop;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.sbt.mipt.oop.homeunits.SmartHome;
+import ru.sbt.mipt.oop.processors.*;
 import ru.sbt.mipt.oop.sensoreventprovider.RandomSensorEventProvider;
 import ru.sbt.mipt.oop.sensoreventprovider.SensorEventProvider;
 import ru.sbt.mipt.oop.sensoreventsmanager.SensorEventsManager;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class MyConfiguration {
@@ -26,7 +29,7 @@ public class MyConfiguration {
     public MyConfiguration() throws IOException {
         smartHome = smartHomeLoader.loadSmartHome();
         sensorEventProvider = new RandomSensorEventProvider();
-        sensorEventsManager = new HomeEventsObserver(smartHome, sensorEventProvider);
+        sensorEventsManager = new HomeEventsObserver(createEventProcessorList(smartHome), sensorEventProvider);
         // If we want to use external library:
         // sensorEventsManager = new AdapterSensorEventsManager();
     }
@@ -35,6 +38,16 @@ public class MyConfiguration {
         MyConfiguration.smartHomeLoader = smartHomeLoader;
     }
 
-
-
+    private static List<EventProcessor> createEventProcessorList(SmartHome smartHome) {
+        List<EventProcessor> eventProcessorList = new ArrayList<>();
+        EventProcessor lightEventProcessor = new DecoratorCheckAlarmEventProcessor(new LightEventProcessor(smartHome), smartHome);
+        EventProcessor doorEventProcessor = new DecoratorCheckAlarmEventProcessor(new DoorEventProcessor(smartHome), smartHome);
+        EventProcessor hallDoorEventProcessor = new DecoratorCheckAlarmEventProcessor(new HallDoorEventProcessor(smartHome), smartHome);
+        AlarmEventProcessor alarmEventProcessor = new AlarmEventProcessor(smartHome);
+        eventProcessorList.add(lightEventProcessor);
+        eventProcessorList.add(doorEventProcessor);
+        eventProcessorList.add(hallDoorEventProcessor);
+        eventProcessorList.add(alarmEventProcessor);
+        return eventProcessorList;
+    }
 }
